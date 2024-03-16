38d0788a2dc2e2c20ce05629888723668d2300c5
Mustafa <yusufmustufa@gmail.com>
2024-03-16 13:08:37 +0530

    Upload page for Kishan

--- TeraByte/src/components/Header.jsx
+++ TeraByte/src/components/Header.jsx
@@ -1,8 +1,21 @@
 /* import React from 'react' */
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <nav>
+      
+      <div className="left">
+        <img src="" alt="logo" />
+        <h1>Photoly</h1>
+      </div>
 
-export default Header
+      <div className="right">
+        <Link to={"/"}> Home </Link>
+        <Link to={"/"}> About </Link>
+        <Link to={"/login"}> Login </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
