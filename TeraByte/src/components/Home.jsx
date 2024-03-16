b70e66e7132c0b07371e59880949fb58da842e13
Mustafa <yusufmustufa@gmail.com>
2024-03-16 17:02:35 +0530

    Starting with home page

--- TeraByte/src/components/Home.jsx
+++ TeraByte/src/components/Home.jsx
@@ -2,7 +2,19 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home1">
+      <div>
+        
+        <h1>About</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores quos totam ipsum pariatur amet neque reprehenderit et quasi eligendi dignissimos, at illo deleniti placeat, quia rem rerum! Officia, enim quos?
+        Laudantium harum impedit ex consequuntur? Deleniti cum perspiciatis voluptatum inventore ut rerum quas deserunt exercitationem dicta saepe reiciendis asperiores nemo eos at totam, nisi quisquam molestiae odit! Labore, explicabo obcaecati?
+        Laudantium fugiat veritatis exercitationem, quas adipisci nam sunt, illo magni iure placeat similique ratione maxime earum saepe, magnam nesciunt aperiam delectus rem? Est, at veritatis excepturi dolorem magni architecto ex.
+        Dolores impedit suscipit tempora veritatis cupiditate eius ea voluptatibus quia consectetur nostrum unde quos rerum ipsam aliquam cumque blanditiis incidunt quisquam velit corrupti praesentium ex et voluptates, ab laborum. Aperiam.
+        Debitis ex amet repellat ipsam. Iste dignissimos error facere dolore beatae ut tempora neque aliquam placeat libero suscipit dolores obcaecati enim quam nisi, quia omnis asperiores assumenda nobis debitis. Consequuntur?</p>
+        
+      </div>
+
+    </div>
   )
 }
 
