6bd5f072677103741d57f5fc8deddeaf308e05e8
rya23 <aaryathakur48@gmail.com>
2024-03-16 18:48:51 +0530

    test

--- TeraByte/src/App.jsx
+++ TeraByte/src/App.jsx
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import  './styles/header.scss'
-import Upload from "./components/Upload";
 
 const App = () => {
   return (
@@ -12,7 +10,6 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/upload" element={<Upload/>} />
       </Routes>
       <Footer />
     </Router>
