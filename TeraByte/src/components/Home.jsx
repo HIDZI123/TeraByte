aa9ef2089c6695c1fb3a9d29db726323880291a8
Mustafa <yusufmustufa@gmail.com>
2024-03-16 18:11:00 +0530

    Some changed

--- TeraByte/src/components/Home.jsx
+++ TeraByte/src/components/Home.jsx
@@ -4,6 +4,8 @@ const Home = () => {
   return (
     <div className="home1">
       <div>
+      
+
         
         <h1>About</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores quos totam ipsum pariatur amet neque reprehenderit et quasi eligendi dignissimos, at illo deleniti placeat, quia rem rerum! Officia, enim quos?
@@ -11,6 +13,7 @@ const Home = () => {
         Laudantium fugiat veritatis exercitationem, quas adipisci nam sunt, illo magni iure placeat similique ratione maxime earum saepe, magnam nesciunt aperiam delectus rem? Est, at veritatis excepturi dolorem magni architecto ex.
         Dolores impedit suscipit tempora veritatis cupiditate eius ea voluptatibus quia consectetur nostrum unde quos rerum ipsam aliquam cumque blanditiis incidunt quisquam velit corrupti praesentium ex et voluptates, ab laborum. Aperiam.
         Debitis ex amet repellat ipsam. Iste dignissimos error facere dolore beatae ut tempora neque aliquam placeat libero suscipit dolores obcaecati enim quam nisi, quia omnis asperiores assumenda nobis debitis. Consequuntur?</p>
+        
         
       </div>
 
