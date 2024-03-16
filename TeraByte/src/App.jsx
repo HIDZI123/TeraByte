b5e23a56f576a6c5bce7f412f43d8116b04b1a23
varunputta1511 <varunputta1511@gmail.com>
2024-03-16 16:19:31 +0530

    test

--- TeraByte/src/App.jsx
+++ TeraByte/src/App.jsx
@@ -1,19 +1,53 @@
-/* import React from 'react' */
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
+import Card from "./Card";
+import Carousel from "./Carousel";
 
-const App = () => {
+function App() {
+  let cards = [
+    {
+      key: uuidv4(),
+      content: (
+        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/convertplus_thumbnail.jpg" />
+      )
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/acf_pro.png" />
+      )
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/layer_slider_plugin_thumb.png" />
+      )
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2016/08/slider_revolution-1.png" />
+      )
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2019/01/pwa_880_660.jpg" />
+      )
+    }
+  ];
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <div className="">
+      <Carousel
+        cards={cards}
+        height="500px"
+        width="30%"
+        margin="0 auto"
+        offset={2}
+        showArrows={false}
+      />
+    </div>
   );
-};
+}
 
 export default App;
